25f13c14a72923f1ce14bc04eacf3d0ec74304af
Brian Alwill <balwill@bu.edu>
2021-03-08 13:16:42 -0500

    Updated PORT env var

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const { findSubscriber }          = require('./sfmcHelper')
 const throng = require('throng')
 const WORKERS = process.env.WEB_CONCURRENCY || 1
 const isDev = process.env.NODE_ENV !== 'production'
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 
 function start() {
   const app = express();
